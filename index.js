be315318ab27e733b8d66c6e933c0fc615e09eda
orange-bottle <144272731+orange-bottle@users.noreply.github.com>
2024-01-21 23:59:44 -0500

    Challenge 6 Unfinished

--- index.js
+++ index.js
@@ -153,10 +153,15 @@ allBtn.addEventListener("click", function() {
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+const unfundedGamesCount = GAMES_JSON.filter(game => game.pledged < game.goal).length;
 
 // create a string that explains the number of unfunded games using the ternary operator
+const totalGames = GAMES_JSON.length;
 
+const displayStr = `
+    A total of $${totalRaised.toLocaleString()} has been raised for ${totalGames} games. Currently, ${unfundedGamesCount > 0 ? 
+        'games remain' : 'game remains'} unfunded. We need your help to fund these amazing games!
+`;
 
 // create a new DOM element containing the template string and append it to the description container
 
